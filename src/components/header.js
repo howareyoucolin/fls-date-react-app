d8560c44e04ae2f7c0481bc0d818f06535ef1e09
Colin ZHao <howareyoucolin@gmail.com>
2020-11-08 06:06:19 +0000

    update header links & refactor css

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './header.scss';
+
 class Header extends React.Component {
+	
+	componentWillMount() {
+		this.baseUrl = '//'+window.location.hostname+'/';
+	}
+	
 	render() {
 		return (
 			<div className="header">
 				<div className="container">
-					<h1><a href="https://www.flushingdating.com">纽约同城交友</a></h1>
-					<a className="signup" href="https://www.flushingdating.com/signup">注册<span>会员</span></a>
-					<a className="signin" href="https://www.flushingdating.com/login">登陆</a>
+					<h1><a href={this.baseUrl}>纽约同城交友</a></h1>
+					<a className="signup" href={this.baseUrl+'signup'}>注册<span>会员</span></a>
+					<a className="signin" href={this.baseUrl+'login'}>登陆</a>
 				</div>
 			</div>
 		)
