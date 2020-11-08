59a9082f0f2e7faa27d55967a202a3496b389068
Colin ZHao <howareyoucolin@gmail.com>
2020-11-08 13:03:17 +0000

    restyle module member

--- src/modules/member.js
+++ src/modules/member.js
@@ -12,19 +12,24 @@ class Member extends React.Component {
 	render() {
 		return (
 			<div className="member">
-				<h2>
-					<a href="https://www.flushingdating.com/member/16">法拉盛理工男诚征女朋友</a>	
-				</h2>
-				<a href="https://www.flushingdating.com/member/16">
-					<img src="https://www.flushingdating.com/uploads/9603e03806ae31df86e279b01a603108.jpg" />
-				</a>
-				<p>
-					Colin Zhao , 18 , 男生<br/>
-					微信: <b>colin6478828</b><br/>
-				</p>
-				<p>
-					身高177厘米,  来自广东, 18岁移民来美国, 在纽约法拉盛住了差不多十多年, 在Queens College读数学与计算机的专业, 从事IT行业, 现在在美国人大公司里担任Senior Software Engineer ...
-					<a href="https://www.flushingdating.com/member/16">[更多]</a>
+				<div className="wrap">
+					<a href={this.baseUrl+'member/16'}>
+						<img src="https://www.flushingdating.com/uploads/9603e03806ae31df86e279b01a603108.jpg" alt="法拉盛理工男诚征女朋友" />
+					</a>
+					<div>
+						<p>
+							Colin Zhao , 18 , 男生<br/>
+							微信: <b>colin6478828</b><br/>
+						</p>
+						<p className="intro">
+							身高177厘米,  来自广东, 18岁移民来美国, 在纽约法拉盛住了差不多十多年, 在Queens College读数学与计算机的专业, 从事IT行业, 现在在美国人大公司里担任Senior Software Engineer ...
+							<a href={this.baseUrl+'member/16'}>[更多]</a>
+						</p>
+					</div>
+				</div>
+				<p className="intro">
+						身高177厘米,  来自广东, 18岁移民来美国, 在纽约法拉盛住了差不多十多年, 在Queens College读数学与计算机的专业, 从事IT行业, 现在在美国人大公司里担任Senior Software Engineer ...
+						<a href={this.baseUrl+'member/16'}>[更多]</a>
 				</p>
 			</div>
 		)
