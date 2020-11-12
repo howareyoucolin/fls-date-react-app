95a3cc8545accc4459065d83aa0082e4f4661177
Colin ZHao <howareyoucolin@gmail.com>
2020-11-12 06:27:35 +0000

    refactor header

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import '~/src/scss/component-header.scss'
 
-import '~/src/scss/component-header.scss';
-
-class Header extends React.Component {
+export default function Header(props){
 	
-	render() {
-		return (
-			<div className="header">
-				<div className="container">
-					<h1><a href='/'>纽约同城交友</a></h1>
-					<a className="signup" href='/signup'>注册<span>会员</span></a>
-					<a className="signin" href='/login'>登陆</a>
-				</div>
+	return (
+		<div className="header">
+			<div className="container">
+				<h1><a href='/'>纽约同城交友</a></h1>
+				<a className="signup" href='/signup'>注册<span>会员</span></a>
+				<a className="signin" href='/login'>登陆</a>
 			</div>
-		)
-	}
-};
-
-export default Header;
+		</div>
+	)
+	
+}
